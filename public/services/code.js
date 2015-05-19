7102f1e3ad7287511f4bc6c303b17953bd2a450e
Orville Fernandes <orville.fernandes@bitbrothers.in>
2015-05-20 01:42:22 +0530

    faulty previous commit fix

--- public/services/code.js
+++ public/services/code.js
@@ -1,10 +1,9 @@
 angular.module('Codegurukul')
     .factory('Codes', function($resource) {
     var Codes = {
-        default: $resource('/api/codes/:cslug/validateCode',{},
-                           {'query': {method:'GET', isArray:true}
-                           }
-                          )
+        default: $resource('/api/codes/:cslug/validateCode',{
+        cslug: '@cslug'
+        })
     }
     return Codes;
 });
