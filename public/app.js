00ad97538fe37b6ff04ef72763d5badd7f807d94
Warren <warr3ngon@gmail.com>
2015-06-01 11:47:25 +0530

    Removed redirection on wrong login creds

--- public/app.js
+++ public/app.js
@@ -118,9 +118,6 @@ angular.module('Codegurukul', ['ngResource', 'ngMessages', 'ui.router', 'mgcrea.
                 return config;
             },
             responseError: function(response) {
-                if (response.status === 401 || response.status === 403) {
-                    $location.path('/login');
-                }
                 if (response.status === 404) {
                     $location.path('home');
                 }
