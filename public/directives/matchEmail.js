92678ac5395cf6c6cb12d345414b333f341df493
Warren <warr3ngon@gmail.com>
2015-06-16 15:03:12 +0530

    bugfix

--- public/directives/matchEmail.js
+++ public/directives/matchEmail.js
@@ -7,6 +7,7 @@ angular.module('Codegurukul')
         element.bind('blur', function() {
           if (ngModel.$modelValue) {
             $http.get('/api/users', { params: { email: ngModel.$modelValue } }).success(function(data) {
+                console.log(data.available+ " match");
               ngModel.$setValidity('match', !data.available);
             });
           }
